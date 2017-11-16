0c024b246339ceafc6d1290ca4c0727948729af2
hassaneodesk <hassaneihammouten@gmail.com>
2017-11-16 12:28:50 +0100

    Structuring css files with the 7-1 Pattern and making some adjustements on pages with sass and also responsive design with mobile first

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,15 +1,20 @@
 var gulp                = require('gulp');
 var autoprefixer        = require('gulp-autoprefixer');
 var sass                = require('gulp-sass');
+var postcss             = require('gulp-postcss');
+var cssImport           = require('postcss-import');
+var cssnano             = require('gulp-cssnano');
 
 gulp.task('styles', () => {
   return gulp.src('app/public/styles/main.scss')
+    .pipe(postcss([cssImport]))
     .pipe(autoprefixer({
       browsers: ['last 5 versions']
     }))
     .pipe(sass({
       outputStyle: 'expanded'
     }))
+    .pipe(cssnano())
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString())
       this.emit('end');
